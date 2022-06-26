42e83068985e2202c6b5325da6ff56018aa90338
RoiYehezkel <roi5634@gmail.com>
2022-06-26 21:49:03 +0300

    card design edited and add to watchlist added

--- src/components/cards/CardItem.js
+++ src/components/cards/CardItem.js
@@ -1,40 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Cards.css";
 
-// function CardItem(props) {
-//   return (
-//     <>
-//       <li class="cards-item">
-//         <Link to={props.path} class="cards-item-link">
-//           <figure class="cards-item-pic-wrap" data-category={props.label}>
-//             <img src={props.src} alt="Travel" class="cards-item-img" />
-//           </figure>
-//           <div class="cards-item-info">
-//             <h5 class="cards-item-text">{props.text}</h5>
-//           </div>
-//         </Link>
-//       </li>
-//     </>
-//   );
-// }
 function CardItem(props) {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <>
-      <li class="cards-item ">
-        <Link to="/" class="cards-item-link">
-          <figure class="cards-item-pic-wrap" data-category="Add to WatchList">
+      <li class="cards-item">
+        {/* <Link> */}
+        <button class="cards-item-link">
+          <button class="cards-item-pic-wrap">
             <img
               class="cards-item-img"
               src={props.data.image}
               alt={props.data.title}
             />
-          </figure>
+          </button>
+
           <div class="cards-item-info">
             <h5 class="cards-item-text">{props.data.title}</h5>
+
             <h6 class="cards-item-date">{props.data.date}</h6>
           </div>
-        </Link>
+        </button>
+        {/* </Link> */}
+        <div class="add-btn-wrapper">
+          <button class="add-btn" onClick={handleClick}>
+            <i
+              class={
+                click ? "fa-solid fa-minus fa-2xl" : "fa-solid fa-plus fa-2xl"
+              }
+            ></i>
+          </button>
+        </div>
       </li>
     </>
   );
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -3,56 +3,13 @@ import CardItem from "./CardItem";
 import { useContext } from "react";
 import loginContext from "../../context/loginContext";
 
-// function Cards() {
-//   return (
-//     <div class="cards">
-//       <h1>Check out these EPIC Destinations!</h1>
-//       <div class="cards-container">
-//         <div class="cards-wrapper">
-//           <ul class="cards-item">
-//             <CardItem
-//               src="./images/img-9.jpg"
-//               text="Explore the hidden waterfall deep inside the Amazon Jungle"
-//               label="Adventure"
-//               path="/Services"
-//             />
-//             <CardItem
-//               src="./images/img-7.jpg"
-//               text="Travel to the iland"
-//               label="Luxury"
-//               path="/Services"
-//             />
-//             <CardItem
-//               src="./images/img-4.jpg"
-//               text="Travel to the iland"
-//               label="Luxury"
-//               path="/Services"
-//             />
-//             <CardItem
-//               src="./images/img-2.jpg"
-//               text="Travel to the iland"
-//               label="Luxury"
-//               path="/Services"
-//             />
-//             <CardItem
-//               src="./images/img-5.jpg"
-//               text="Travel to the iland"
-//               label="Luxury"
-//               path="/Services"
-//             />
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 function Cards() {
   const { movies } = useContext(loginContext);
   return (
     <div class="cards">
       <div class="cards-container">
         <div class="cards-wrapper">
-          <ul class="cards-item">
+          <ul class="cards-items">
             {movies.map((movie) => (
               <CardItem data={movie} />
             ))}
