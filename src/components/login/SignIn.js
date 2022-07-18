eb0b7f54160342fb18766528e0291d708b223e3e
RoiYehezkel <roi5634@gmail.com>
2022-07-18 16:03:58 +0300

    style for phone view wdited

--- src/components/login/SignIn.js
+++ src/components/login/SignIn.js
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import GlobalContext from "../../globalContext/GlobalContext";
 import "./Login.css";
 
+// login text render in the navbar
 function SignIn() {
   const { setLogin, signed, name, handleMovieToWatchlist } =
     useContext(GlobalContext);
